78c94336d7ba11d55236fa77c7ba4914c6ea5e7f
bellalavinaykumar <73272386+bellalavinaykumar@users.noreply.github.com>
2021-04-13 11:27:10 +0530

    python tutorials

--- pythonproject.py
+++ pythonproject.py
@@ -1,5 +1,18 @@
-print("hello python")
-print("vinay kumar bellala")
-for i in range(10):
-  print("vinay")
-#added one line
+from array import *
+
+arr1=array("i",[])
+
+n=int(input("enter the length of array : "))
+
+for i in range(n):
+  k=int(input("enter the value of array : "))
+  arr1.append(k)
+print(arr1)
+
+num_search=int(input("enter the search value : "))
+
+a=0
+for i in arr1:
+  if num_search==i:
+    print(a)
+  a+=1
---
+++ vinayproject.py
@@ -0,0 +1,38 @@
+def data_types():
+    "none"
+    "numaric"
+    "string"
+    "range"
+    "list"
+    "tuples"
+    "set"
+    "map"
+
+def operators():
+    "arthematic"
+    "assigment"
+    "locical"
+    "urinary"
+    "relationl"
+
+def number_system_conversation():
+    "decimal"
+    "binary"
+    "hexa decimal"
+    "octa decimal"
+
+def bitwise_operator():
+    "complimentary"
+    "or"
+    "and"
+    "xor"
+    "lift shift"
+    "right shift"
+def user_input():
+    "eval"
+    "argv comand input"
+
+def printing_prime_numbers():
+    pass
+def arrays():
+

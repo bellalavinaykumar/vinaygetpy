83e67392e25d1354e97b57ce9596f0d167241801
bellalavinaykumar <73272386+bellalavinaykumar@users.noreply.github.com>
2021-06-20 11:36:35 +0530

    python pratice
    bitwise operator

--- pythonproject.py
+++ pythonproject.py
@@ -1,21 +1,9 @@
-class student:
-    def __init__(self,name,roll,class_room):
-        self.name=name
-        self.roll=roll
-        self.class_room=class_room
+#Bitwise operator
+#decimal 0-9
+#binary 0-1
+#octa decimal 0-7
+#hex decimal 0-9,a-f
 
-    def math_marks(self,mark):
-        self.marks=mark
-        return self.marks
-    def comparing_marks(self,other):
-        if self.math_marks>other.math_marks:
-            print(self.name,"wins")
-        else:
-            print(other.name,"wins")
-
-s1=student("ram",51,"4th")
-s2=student("vinay",50,"4th")
-print(s1.name,s2.name)
-print(s1.math_marks(54))
-print(s2.math_marks(58))
-
+print(bin(50))
+print(oct(50))
+print(hex(50))

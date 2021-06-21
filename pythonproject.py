cbf148ac88e0b1edcb837df34aed6b70db9f05e4
bellalavinaykumar <73272386+bellalavinaykumar@users.noreply.github.com>
2021-06-21 12:25:43 +0530

    python pratice
    #loops
    #while loop
    #Nested while loop
    #for loop
    #nested for loop

--- pythonproject.py
+++ pythonproject.py
@@ -1,9 +1,13 @@
-#Bitwise operator
-#decimal 0-9
-#binary 0-1
-#octa decimal 0-7
-#hex decimal 0-9,a-f
-
-print(bin(50))
-print(oct(50))
-print(hex(50))
+#loops
+#while loop
+#for loop
+#nested loops
+i=5
+while i>=1:
+    print("vinay ",end="")
+    j=4
+    while j>=1:
+        print("kumar",end=" ")
+        j-=1
+    print()
+    i-=1

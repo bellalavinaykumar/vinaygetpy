e2d62b0ee30592a567332ef4d26856f0858057e4
bellalavinaykumar <73272386+bellalavinaykumar@users.noreply.github.com>
2021-06-21 12:29:21 +0530

    python pratice
    #loops
    #while loop
    #Nested while loop

--- pythonproject.py
+++ pythonproject.py
@@ -2,12 +2,15 @@
 #while loop
 #for loop
 #nested loops
-i=5
-while i>=1:
-    print("vinay ",end="")
-    j=4
-    while j>=1:
-        print("kumar",end=" ")
-        j-=1
-    print()
-    i-=1
+
+def while_loop(n,n1):
+    i=n
+    while i>=1:
+        print("nested ",end="")
+        j=n1
+        while j>=1:
+            print("loop",end=" ")
+            j-=1
+        print()
+        i-=1
+while_loop(4,1)

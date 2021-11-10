a24bfec0d48694e37d4988a82ce0b30744e1134b
vinay kumar bellala <vinaykumar24410@gmail.com>
2021-11-10 12:02:48 +0530

    to count the number of even and odd numbers from a series of numbers.
    #to count the number of even and odd numbers from a series of numbers.

--- vinayproject.py
+++ vinayproject.py
@@ -1,2 +1,10 @@
-val=input("enter the String value ")
-print(val[::-1])
+list=(1,2,3,4,5,6,7,8,9)
+odd_count=0
+even_count=0
+for i in list:
+    if i%2==0:
+        even_count+=1
+    else:
+        odd_count+=1
+print(f'Total Odd Numbers Present in list {odd_count} ')
+print(f'Total even Numbers Present in list {even_count} ')

0d7acf03c97a59f974c98868df07b74fc2965a95
vinay kumar bellala <vinaykumar24410@gmail.com>
2021-11-10 11:22:40 +0530

    the Words To Mirror Dimension
    #Mirror Dimension

--- vinayproject.py
+++ vinayproject.py
@@ -1,14 +1,2 @@
-def Fibonacci(n):
-    if n==1 or n==0:
-        print(0)
-    else:
-        a=0
-        b=1
-        print(a)
-        print(b)
-        for i in range(2,n):
-            c=a+b
-            a=b
-            b=c
-            print(c)
-Fibonacci(4)
+val=input("enter the String value ")
+print(val[::-1])
